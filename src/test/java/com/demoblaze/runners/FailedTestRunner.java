fc58ede9ec7dc8286fdf3649ff3a45ee5b302144
Mustafa Kemal <kemalaltun7108@gmail.com>
2023-06-14 03:22:15 +0300

    First commit

---
+++ src/test/java/com/demoblaze/runners/FailedTestRunner.java
@@ -0,0 +1,20 @@
+package com.demoblaze.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/failed-html-reports",
+        },
+        features = "@target/rerun.txt",
+        glue = "com/demoblaze/step_defs",
+        dryRun = false,
+        tags = ""
+)
+
+
+public class FailedTestRunner {
+}
